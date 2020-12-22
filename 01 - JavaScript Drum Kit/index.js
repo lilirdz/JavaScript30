539b2050e391495fe0180fbd6255df2d7033c129
lilirdz <lilimrod@gmail.com>
2020-12-22 00:22:01 -0600

    cleaned up code and app is fully functioning now

--- 01 - JavaScript Drum Kit/index.js
+++ 01 - JavaScript Drum Kit/index.js
@@ -1,5 +1,19 @@
-window.addEventListener('keydown', function(e){
+function playSound(e) {
     const audio = document.querySelector(`audio[data-key=${e.key}]`)
-    console.log(audio)
-    // console.log(e.key)
-})
+    const key = document.querySelector(`.key[data-key=${e.key}]`)
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing')
+}
+
+function removeTransition(e) {
+    if(e.propertyName !== 'transform') return;
+    this.classList.remove('playing')
+}
+
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+window.addEventListener('keydown', playSound)
+
+
